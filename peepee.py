40fc665742fc1334ca8cf7c05af57d061c2c99a0
theres-a-bea <samuelsson.bea@gmail.com>
2021-11-06 22:17:27 +0800

    First Push

---
+++ peepee.py
@@ -0,0 +1,22 @@
+
+import os
+
+csvin = 'C:\\Users\\hi\\Documents\\repos\\playlist-downloader\\testcsv2.csv'
+
+path, file = os.path.split(csvin)
+
+tmp = file.split('.')
+
+file = tmp[0]
+
+import os
+
+csvin = 'C:\\Users\\hi\\Documents\\repos\\playlist-downloader\\testcsv2.csv'
+
+path, file = os.path.split(csvin)
+
+tmp = file.split('.')
+
+file = tmp[0]
+
+print (file)
